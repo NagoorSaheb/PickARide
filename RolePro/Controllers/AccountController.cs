50aac1c1e2af7ca2af59e928c5ab31518f9cbe29
Nagoor <nshaik2@uh.edu>
2016-09-05 20:23:25 -0500

    uploading

---
+++ RolePro/Controllers/AccountController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using RolePro.Models.ViewModel;
+using RolePro.Models.EntityManager;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace RolePro.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(UserSignUpView USV)
+        {
+            if (ModelState.IsValid)
+            {
+                UserManager UM = new UserManager();
+                if (!UM.IsLoginNameExist(USV.LoginName))
+                {
+                    UM.AddUserAccount(USV);
+                    FormsAuthentication.SetAuthCookie(USV.FirstName, false);
+                    return RedirectToAction("Welcome", "Home");
+
+                }
+                else
+                    ModelState.AddModelError("", "Login Name already taken.");
+            }
+            return View();
+        }
+
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogIn(UserLoginView ULV, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                UserManager UM = new UserManager();
+                string password = UM.GetUserPassword(ULV.LoginName);
+
+                if (string.IsNullOrEmpty(password))
+                    ModelState.AddModelError("", "The user login or password provided is incorrect.");
+                else
+                {
+                    if (ULV.Password.Equals(password))
+                    {
+                        FormsAuthentication.SetAuthCookie(ULV.LoginName, false);
+                        return RedirectToAction("Welcome", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "The password provided is incorrect.");
+                    }
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(ULV);
+        }
+
+
+        [Authorize]
+        public ActionResult SignOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+        // GET: Account
+        public ActionResult RequestRide()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RequestRide(UserRequestRideView USRV)
+        {
+            Console.WriteLine("Came from html page "+USRV.SYSUserId);
+
+          
+
+            if (ModelState.IsValid)
+            {
+                UserManager UM = new UserManager();
+              
+                   UM.RequestRide(USRV);
+                   return RedirectToAction("Welcome", "Home");
+
+                }
+                else
+                    ModelState.AddModelError("", "Request Cant be processed at this time.");
+            
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult SubmitRequest(string userID, string phone, string persons, string comments)
+        {
+                     
+            UserRequestRideView USRV = new UserRequestRideView();
+           
+            USRV.AddPhoneNumber = phone;
+            USRV.Comments = comments;
+            USRV.NumberOfPersons = persons;
+            USRV.loginName = userID;
+
+            Console.WriteLine("Came from html page " + userID);
+            Console.WriteLine("Number received from page " + phone);
+            Console.WriteLine("comments from page " + userID);
+            UserManager UM = new UserManager();
+              
+                   UM.RequestRide(USRV);
+
+            return Json(new { success = true });
+        }
+    }
+}
---
+++ RolePro/Models/ViewModel/UserModel.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace RolePro.Models.ViewModel
+{
+    public class UserSignUpView
+    {
+        [Key]
+        public int SYSUserID { get; set; }
+        public int LOOKUPRoleID { get; set; }
+        public string RoleName { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Login ID")]
+        public string LoginName { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Phone Number")]
+        public string phoneNumber { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Email Id")]
+        public string emailId { get; set; }
+        public string Gender { get; set; }
+    }
+
+
+    public class UserRequestRideView
+    {
+        [Key]
+
+        [Required(ErrorMessage = "*")]
+        public string SYSUserId { get; set; }
+        public string loginName { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string NumberOfPersons { get; set; }
+        [Display(Name = "Additional Contact Number")]
+        public string AddPhoneNumber { get; set; }
+        [Display(Name = "Estimated Charge")]
+        public string Charge{ get; set; }
+        public string Comments { get; set; }
+        [Display(Name = "Status")]
+        public string RequestStatus { get; set; }
+       
+    }
+
+
+
+    public class UserLoginView
+    {
+        [Key]
+        public int SYSUserID { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Login ID")]
+        public string LoginName { get; set; }
+        [Required(ErrorMessage = "*")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+
+    public class UserProfileView
+    {
+        [Key]
+        public int SYSUserID { get; set; }
+        public int LOOKUPRoleID { get; set; }
+        public string RoleName { get; set; }
+        public bool? IsRoleActive { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Login ID")]
+        public string LoginName { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+          [Required(ErrorMessage = "*")]
+        [Display(Name = "Phone Number")]
+        public string phoneNumber { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Email Id")]
+        public string emailId { get; set; }
+        public string Gender { get; set; }
+    }
+
+    public class LOOKUPAvailableRole
+    {
+        [Key]
+        public int LOOKUPRoleID { get; set; }
+        public string RoleName { get; set; }
+        public string RoleDescription { get; set; }
+    }
+
+    public class Gender
+    {
+        public string Text { get; set; }
+        public string Value { get; set; }
+    }
+    public class UserRoles
+    {
+        public int? SelectedRoleID { get; set; }
+        public IEnumerable<LOOKUPAvailableRole> UserRoleList { get; set; }
+    }
+
+    public class UserGender
+    {
+        public string SelectedGender { get; set; }
+        public IEnumerable<Gender> Gender { get; set; }
+    }
+
+    public class UserDataView
+    {
+        public IEnumerable<UserProfileView> UserProfile { get; set; }
+        public UserRoles UserRoles { get; set; }
+        public UserGender UserGender { get; set; }
+    }
+
+}
